b80959e25b29a64157160b0407f493103971eb6d
AminEsmaeily <s.amin.esmaeily@gmail.com>
2020-11-07 14:12:31 +0330

    WIP:Developing datepicker

---
+++ Lava/LavaBaseComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Lava
+{
+    public class LavaBaseComponent : ComponentBase
+    {
+        private bool _isFocused = false;
+        [Parameter]
+        public bool IsFocused
+        {
+            get => _isFocused;
+            set
+            {
+                if (_isFocused == value)
+                    return;
+
+                _isFocused = value;
+                OnIsFocusedChanged();
+                IsFocusedChanged.InvokeAsync(value);
+            }
+        }
+        [Parameter]
+        public EventCallback<bool> IsFocusedChanged { get; set; }
+
+        protected virtual void OnIsFocusedChanged()
+        {
+            // This part could be overridden in child classes
+        }
+    }
+}
